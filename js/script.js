6986cc96a9d7bd6fa17cd93d2e568ab675b31e00
Kamil Trzcinski <asasn233@gmail.com>
2023-11-17 18:25:52 +0100

    comments & player move

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
+// variables
 var computerMove;
 var playermove;
-playermove = 'papier';
 var randomNumber;
+var playerInput;
+
+// random number
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
+
+// computer move
 if (randomNumber == '1') {
     computerMove = 'kamień';
   } else  if (randomNumber == '2') {
@@ -12,5 +17,18 @@ if (randomNumber == '1') {
     computerMove = 'nożyce';
   }
 
+// player move
+playerInput = prompt('Wybierz swój ruch! 1: Kamień, 2: Papier, 3: Nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playermove = 'kamień';
+} else  if (playerInput == '2') {
+  playermove = 'papier';
+} else if (playerInput == '3') {
+  playermove = 'nożyce';
+} else {
+  playermove = 'Wybrałeś liczbę inną niż 1,2,3...'
+}
 
-printMessage('Zagrałem ' + computerMove + '! Twój ruch to ' + playermove + '! Wygrywasz !');
+// print results
+printMessage('Zagrałem ' + computerMove + '! Twój ruch to ' + playermove);
