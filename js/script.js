c114cce5433f5b1cf72457e8ea6281ceda5aeaab
Kamil Trzcinski <asasn233@gmail.com>
2023-11-17 18:16:20 +0100

    add random number & else if

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 var computerMove;
-computerMove = 'kamień';
 var playermove;
 playermove = 'papier';
+var randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+    computerMove = 'kamień';
+  } else  if (randomNumber == '2') {
+    computerMove = 'papier';
+  } else {
+    computerMove = 'nożyce';
+  }
+
+
 printMessage('Zagrałem ' + computerMove + '! Twój ruch to ' + playermove + '! Wygrywasz !');
